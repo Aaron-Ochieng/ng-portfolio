c11267ad3ab347ad4cdfb6706a57b994df52d854
Aaron Ochieng <ochiengaoron@outlook.com>
2026-02-02 10:59:11 +0300

    feat: replace FontAwesome with Angular Material SVG icons and update typography
    - Switched social icons from FontAwesome to Angular Material mat-icon
      with custom SVGs
    - Registered custom SVG icons using MatIconRegistry and DomSanitizer
    - Added JetBrains Mono font styling and applied it to header and intro
      text
    - Added section-specific CSS for icon sizing and font normalization
    - Updated social links and improved template formatting

--- src/app/components/section-a/section-a.ts
+++ src/app/components/section-a/section-a.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faDev, faGithub, faLinkedin, faSquareXTwitter} from '@fortawesome/free-brands-svg-icons';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-section-a',
-  imports: [RouterOutlet,FontAwesomeModule],
+  imports: [RouterOutlet,MatIconModule ],
   templateUrl: './section-a.html',
+  styleUrls: ['./section-a.css'],
 })
 export class SectionA {
-  github = faGithub
-  fadev = faDev
-  linkedin = faLinkedin
-  x = faSquareXTwitter
+  constructor(
+    private matIconRegistry:MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon('dev', this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/svg/dev.svg'))
+    this.matIconRegistry.addSvgIcon('x', this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/svg/x.svg'))
+    this.matIconRegistry.addSvgIcon('github', this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/svg/github.svg'))
+    this.matIconRegistry.addSvgIcon('linkedin', this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/svg/linkedin.svg'))
+   }
 }
